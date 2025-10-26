dbc5afd33cc94801143fbbc61f9887c3769286f3
shehryardurrani <shehryardurrani@hotmail.co.uk>
2025-10-26 22:36:56 +0000

    Added code for roman numerals up to 10

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const numToRoman = () => {};
+// Our converter function
+const Roman = ['X', 'IX', 'V', 'IV', 'I'];
+const arabic = [10, 9, 5, 4, 1];
 
-module.exports = {numToRoman}
+const numToRoman = (num) => {
+    let result = '';
+    arabic.map((value, index) => {
+        while(num >= value) {
+            result += Roman [index];
+            num -= value;
+        }
+    });
+    return result;
+};
+
+// Exporting the function to be used in another file
+module.exports = {numToRoman};
--- index.test.js
+++ index.test.js
@@ -1,6 +1,40 @@
+// Roman Numerals converter, with Ping-Pong TDD
 const{numToRoman} = require('./index');
 
+// Smoke Test - Checks that jest is working
 test('That Jest is working', () => {
     expect(2+2).toBe(4);
 });
 
+describe("tests function numToRoman", () => {
+    test("that 1 is I in Roman Numerals", () => {
+        expect(numToRoman(1)).toBe("I");
+    });
+    test("that 2 is II in Roman Numerals", () => {
+        expect(numToRoman(2)).toBe("II");
+    });
+    test("that 3 is III in Roman Numerals", () => {
+        expect(numToRoman(3)).toBe("III");
+    });
+    test("that 4 is IV in Roman Numerals", () => {
+        expect(numToRoman(4)).toBe("IV");
+    });
+    test("that 5 is V in Roman Numerals", () => {
+        expect(numToRoman(5)).toBe("V");
+    });
+    test("that 6 is VI in Roman Numerals", () => {
+        expect(numToRoman(6)).toBe("VI");
+    });
+    test("that 7 is VII in Roman Numerals", () => {
+        expect(numToRoman(7)).toBe("VII");
+    });
+    test("that 8 is VIII in Roman Numerals", () => {
+        expect(numToRoman(8)).toBe("VIII");
+    });
+    test("that 9 is IX in Roman Numerals", () => {
+        expect(numToRoman(9)).toBe("IX");
+    });
+    test("that 10 is X in Roman Numerals", () => {
+        expect(numToRoman(10)).toBe("X");
+    });
+})
